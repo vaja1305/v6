e7420540d5b8892e3d666d1a7ecb8e333dd2ce31
vaja1305 <35728453+vaja1305@users.noreply.github.com>
2018-02-04 15:50:14 +0530

    Create +ve or -ve.c

---
+++ +ve or -ve.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the n value :");
+scanf("%d",&a);
+if(a>=0)
+printf("the given no is positive ");
+else
+printf("the given no is negative ");
+}
